7b9c4d3bb8011f0feefd27f3c277bc845acf9ccc
Syromyatnikov Vyacheslav <zalll007@yandex.ru>
2023-05-26 16:38:17 +0700

    users Schema v0.0.1

--- app_server/models/users.js
+++ app_server/models/users.js
@@ -1,10 +1,53 @@
 const mongoose = require('mongoose');
 
 const usersSchema = new mongoose.Schema({
-    first_name: { type: String, required: true, maxLength: 100 },
-    family_name: { type: String, required: true, maxLength: 100 },
-    father_name: { type: String, maxLength: 100 },
-    date_of_birth: { type: Date, required: true },
-    email: { type: String, required: true },
-
+    firstName: { type: String, required: true, maxLength: 100 },
+    familyName: { type: String, required: true, maxLength: 100 },
+    fatherName: { type: String, maxLength: 100 },
+    nickName: {type: String, required: true, unique: true},
+    dateOfBirth: { type: Date, required: true },
+    email: { type: String, required: true, lowercase: true },
+    password: {type: String}, //some secuirity token ofc
+    currentStage: {
+        start: Date,
+        end: Date,
+        target: Number,
+        groupName: String,
+        reviews: [String] // bookAuthor, bookName, reviewText, createdAt, updatedAt, rating {points, emoji, createdAt, moderator{ name, id}}
+    },
+    oldStages: [String], //array of objects like currentStage
+    liveLocation: {
+        coord: [Number, Number],
+        city: String,
+        region: String
+    },
+    createdAt: Date,
+    updatedAt: Date
 });
+
+usersSchema.pre('save', function(next) {
+    const now = Date.now();
+
+    this.updatedAt = now;
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
+
+    next();
+})
+
+let UserModel = mongoose.model('User', usersSchema);
+
+
+
+let model = new UserModel(
+{
+    firstName: "Вячеслав",
+    familyName: "Сыромятников",
+    dateOfBirth: new Date("1995-02-12"),
+    email: 'zalll007@yandex.ru',
+    nickName: "slava"
+}
+);
+
+model.save();
